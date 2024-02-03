aca621aa6c424fc76df0a31eb2b37565ad0a3471
ab-96 <abdul.123@live.co.uk>
2024-02-03 17:46:36 +0000

    added styling to header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        <h1>Movie Tracking System</h1>
-    </div>
+    <header className='flex justify-center items-center  bg-dBlue h-20'>
+        <h1 className='text-white text-4xl font-bold font-nunito file:'>Movie Tracking System</h1>
+    </header>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+    fontFamily: {
+      nunito: ["Nunito Sans", "sans-serif"],
+    },
+    colors: {
+      dBlue: "hsl(209, 23%, 22%)",
+      vDBlue: "hsl(207, 26%, 17%)",
+      white: "hsl(0, 0%, 100%)",
+      dGrey: " hsl(0, 0%, 52%)",
+      lGrey: "hsl(0, 0%, 98%)",
+    },
+  },
+  plugins: [],
+}
+
